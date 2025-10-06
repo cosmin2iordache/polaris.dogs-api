0b3976e34c0f89e343e3b0297d0b92841bcce911
Cosmin I <cosmin2iordache@gmail.com>
2025-10-06 21:51:15 +0100

    refactoring stuff

--- src/main/java/com/polarissoftware/dogs_api/mapper/DogMapper.java
+++ src/main/java/com/polarissoftware/dogs_api/mapper/DogMapper.java
@@ -2,7 +2,6 @@ package com.polarissoftware.dogs_api.mapper;
 
 import com.polarissoftware.dogs_api.dto.DogDTO;
 import com.polarissoftware.dogs_api.entity.Dog;
-import com.polarissoftware.dogs_api.entity.Supplier;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
